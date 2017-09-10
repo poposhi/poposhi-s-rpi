74a0b925cf869c3e3b07889825e4512a6b7cbfdf
Morvan Zhou <kz85430gmail>
2017-09-10 14:48:01 +0800

    uploadarray

--- git_test.py
+++ git_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import pymysql.cursors
+import datetime
+import numpy as np
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -13,18 +15,25 @@ connection = pymysql.connect(host='localhost',
 try:    #再給表格名 與資料名稱
     with connection.cursor() as cursor:
         # Create a new record  write sql_labguage 
-        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-
+        #sql = "INSERT INTO `motor` (`v`, `a`) VALUES (%s, %s)"
+        #cursor.execute(sql, (1,2 ))
+        
+        time =datetime.datetime.now().isoformat()  #時間
+        cursor.execute('INSERT INTO `motor` (`datetimeee`) VALUES(%s)', (time,))
+       
+        arr= a= np.array_str(np.arange(300))
+        cursor.execute('INSERT INTO `motor` (`longtextttt`,`datetimeee`) VALUES(%s,%s)',
+                       (arr,time))
+        
     # connection is not autocommit by default. So you must commit to save
     # your changes.
     connection.commit()
 
-    with connection.cursor() as cursor:
+    '''with connection.cursor() as cursor:
         # 我要選取id passward 從表格users 的email 
         sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
         cursor.execute(sql, ('webmaster@python.org',))
         result = cursor.fetchone()
-        print(result)
+        print(result)'''
 finally:
     connection.close()
